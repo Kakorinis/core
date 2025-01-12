e6ea6d1ab3e264d711e1fc223c363e407b06846b
kakorinis <kakorin-faugi@mail.ru>
2025-01-12 21:40:02 +0300

    Вынос метода сборки api-приложения в библиотечный метод в utils

--- core/utils/common.py
+++ core/utils/common.py
@@ -1,6 +1,12 @@
+from typing import List
+from typing import Type
+
 from bcrypt import checkpw
 from bcrypt import gensalt
 from bcrypt import hashpw
+from dependency_injector import containers
+from fastapi import APIRouter
+from fastapi import FastAPI
 
 
 def make_hashed_pwd(password: str) -> str:
@@ -20,3 +26,28 @@ def is_the_same_passwords(password: str, hashed: str) -> bool:
     :return: Верно ли указан пароль
     """
     return checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
+
+
+def create_app(depends_container: Type[containers.DeclarativeContainer], routers: List[APIRouter]) -> FastAPI:
+    container = depends_container()
+
+    # FastAPI и Starlette реализуют OPTIONS поддержку, но в запросах должны быть установлены оба заголовка Origin и Access-Control-Request-Method
+    # https://github.com/fastapi/fastapi/issues/1849
+    # origins = ["http://newstyle.ru:8092"]
+    # middleware = [
+    #     Middleware(
+    #         CORSMiddleware,
+    #         allow_origins=["*"],  # origins если [*], то в чем смысл защиты?
+    #         allow_credentials=True,
+    #         allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    #         allow_headers=['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length',
+    #                        'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version', 'Authorization']
+    #     )
+    # ]
+
+    # application = FastAPI(middleware=middleware)
+    application = FastAPI()
+    application.container = container
+    for router in routers:
+        application.include_router(router)
+    return application
